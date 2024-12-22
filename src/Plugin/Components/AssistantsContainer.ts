df33b4a82427d30287daaace9fed38f91af6d759
Evan Harris <echarris@smcm.edu>
2024-12-22 15:36:10 -0500

    Remove redundant temperature setting and swap it for top p

--- src/Plugin/Components/AssistantsContainer.ts
+++ src/Plugin/Components/AssistantsContainer.ts
@@ -556,9 +556,9 @@ export class AssistantsContainer {
 			});
 
 		new Setting(parentContainer)
-			.setName("Temperature")
+			.setName("Top p")
 			.setDesc(
-				"Defaults to 1. What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both."
+				"Defaults to 1. An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered."
 			)
 			.addText((text) => {
 				if (assistant) text.setValue(`${assistant.top_p}`);
